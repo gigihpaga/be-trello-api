bf63c8e644016e356dd82be68cb19dbed46d12a3
gigihpaga <gigihpaga@gmail.com>
2023-02-23 12:40:12 +0700

    menambah middleware untuk validation request API

--- app/api/todos/rooter.js
+++ app/api/todos/rooter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getAll, create, getOne, update, destroy } = require('./controller');
+const { validateCreate, validateGetOne, validateUpdate } = require('./validation');
 
 router.get('/todos', getAll);
-router.post('/todos', create);
-router.get('/todos/:id', getOne);
-router.put('/todos/:id', update);
-router.delete('/todos/:id', destroy);
+router.post('/todos', validateCreate, create);
+router.get('/todos/:id', validateGetOne, getOne);
+router.put('/todos/:id', validateUpdate, update);
+router.delete('/todos/:id', validateGetOne, destroy);
 
 module.exports = router;
